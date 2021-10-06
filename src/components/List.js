12dfcb8b1b8022ae863d558b36f988212bea0918
DESKTOP-PLQ2MBG\Pensar <contact@yusufhayirli.com>
2021-10-06 15:25:48 +0300

    ...

---
+++ src/components/List.js
@@ -0,0 +1,16 @@
+import react, { useState } from "react";
+
+const List = () => {
+
+  const [list, setList] = useState([]);
+
+  return (
+    <div>
+      
+      
+
+    </div>
+  )
+}
+
+export default List;
